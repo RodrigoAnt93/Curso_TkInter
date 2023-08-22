86513d3e83f3d9a64c7f8b41f755c0f28a44cb00
Rodrigo Antonio <rodrigoscfc@gmail.com>
2023-08-22 19:02:35 -0300

    Create Aula_23.py

---
+++ Aula_23.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+class MinhaFrame(Frame):
+    
+    def __init__(self, parent):
+        super().__init__()
+
+        self['bd'] = 1
+        self['relief'] = SOLID
+
+        self.text1_text = StringVar()
+        self.label1_text = StringVar()
+
+        #widgets
+        self.label1 = Label(self, textvariable=self.label1_text).grid()
+        text1 = Entry(self, textvariable=self.text1_text).grid()
+        cmd1 = Button(self, text='Clique', command=self.Executar).grid()
+
+
+    def Executar(self):
+        self.label1_text.set(f'Olá, {self.text1_text.get()}.')
+
+
+root = Tk()
+root.geometry('300x150')
+
+frm1 = MinhaFrame(root).grid()
+frm2 = MinhaFrame(root).grid()
+frm3 = MinhaFrame(root).grid(row=0, column=1)
+
+root.mainloop()
